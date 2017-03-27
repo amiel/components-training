e9287c278de45dc4f5cfa1bb0e15ea0c7f9386b5
Amiel Martin <amiel@actionsprout.com>
2017-03-27 16:11:44 -0700

    Unset weather while loading

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
 
   loadWeather: task(function* (zip) {
     if (zip.length === 5) {
+      this.set('weather', null);
       let weather = yield Ember.$.get('/weather', { zip: zip });
       this.set('weather', weather);
     }
