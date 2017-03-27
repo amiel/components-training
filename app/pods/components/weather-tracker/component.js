4a808dd0026de0fca9453973c3a3aec631eeec63
Amiel Martin <amiel@actionsprout.com>
2017-03-27 15:48:18 -0700

    Load weather without ember-concurrency

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -2,4 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+
+  actions: {
+    loadWeather(zip) {
+      if (zip.length === 5) {
+        this.set('weather', 'Loading...');
+
+        Ember.$.get('/weather').then((weather) => {
+          this.set('weather', weather);
+        });
+
+      }
+    },
+  },
+
 });
