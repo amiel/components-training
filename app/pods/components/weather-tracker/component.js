5031d7eb48bd2c04c27c4eaeb7af4351d4fde799
Amiel Martin <amiel@actionsprout.com>
2017-03-27 16:09:37 -0700

    Use the perform helper for ember-concurrency

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -4,16 +4,9 @@ import { task } from 'ember-concurrency';
 export default Ember.Component.extend({
 
   loadWeather: task(function* (zip) {
-    let weather = yield Ember.$.get('/weather', { zip: zip });
-    this.set('weather', weather);
+    if (zip.length === 5) {
+      let weather = yield Ember.$.get('/weather', { zip: zip });
+      this.set('weather', weather);
+    }
   }).restartable(),
-
-  actions: {
-    loadWeather(zip) {
-      if (zip.length === 5) {
-        this.get('loadWeather').perform(zip);
-      }
-    },
-  },
-
 });
