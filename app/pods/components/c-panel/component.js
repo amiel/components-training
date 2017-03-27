1625bd31824d0f5b53aadc199722e4759ea900a6
Amiel Martin <amiel@actionsprout.com>
2017-03-27 14:22:10 -0700

    Add defaults

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isOpen: true,
 
+  title: 'Default block',
+  body: 'Default body',
+
+
   actions: {
     toggleBody() {
       this.toggleProperty('isOpen');
