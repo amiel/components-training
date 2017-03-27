21d97d15f38b5400332bc5824e91c0470f170b60
Amiel Martin <amiel@actionsprout.com>
2017-03-27 13:41:15 -0700

    Collapse panel when clicked

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showBody: true,
+
+  actions: {
+    toggleBody() {
+      this.toggleProperty('showBody');
+    },
+  },
 });
