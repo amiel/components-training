bf6c768d503ab02abb2eedf3d9f20b43a6e29b34
Amiel Martin <amiel@actionsprout.com>
2017-04-04 15:12:14 -0800

    Implement flyable/flyable-open

--- app/pods/components/x-square/component.js
+++ app/pods/components/x-square/component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['X-square']
+  classNames: ['X-square', 'flyable'],
+  classNameBindings: ['isOpen:flyable-open'],
 });
