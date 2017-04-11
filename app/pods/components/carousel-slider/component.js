881d0dc0da3ae6deeb24676fa17f7ebaed707b4d
Amiel Martin <amiel@actionsprout.com>
2017-04-11 14:05:22 -0800

    Implement each and displayShift from video

--- app/pods/components/carousel-slider/component.js
+++ app/pods/components/carousel-slider/component.js
@@ -6,14 +6,24 @@ export default Ember.Component.extend({
 
   selectedIndex: 0,
 
-  currentSlide: Ember.computed(function() {
+  currentSlide: Ember.computed('selectedIndex', function() {
+    return this.get('slides').get(this.get('selectedIndex'));
   }),
 
-  displayShift: Ember.computed('', function() {
+  displayShift: Ember.computed('selectedIndex', function() {
+    let i = this.get('selectedIndex');
+    let width = 80;
+    let margin = 3;
+    let offset = 15.5;
+
+    let shifted = ((width + margin) * -i) + offset;
+
+    return Ember.String.htmlSafe(`transform: translateX(${shifted}%)`);
   }),
 
   actions: {
-    selectSlide() {
+    selectSlide(index) {
+      this.set('selectedIndex', index);
     }
   }
 
